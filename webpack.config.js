4f2c8deb6ec7fd9d560bc15da67ee254ed3e5030
Jon Lucas <jon.s.lucas@gmail.com>
2016-02-23 20:45:44 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+/* jshint strict:implied */
+/* globals module */
+module.exports = {
+  entry: ['./client/js/client.js'],
+  output: {
+      path: './dist',
+      filename: 'bundle.js',
+      publicPath: '/'
+  },
+  module: {
+      loaders: [
+          {
+              test: /\.js$/,
+              loader: 'babel-loader',
+              exclude: /node_modules/,
+              query: {
+                  presets: ['react', 'es2015']
+              }
+          }
+      ]
+  }
+};
